1fd66108fd7a4e7230099a0fb00c7f4982753efe
Iqbal Mohammad Abdul Ghoni <iqballmags@gmail.com>
2021-08-08 17:25:10 +0700

    Add how to setup

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = require('./src/app')
 app
   .start({
     mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017',
-    mongoDatabase: process.env.MONGO_DATABASE || 'github-review-point-counter-test'
+    mongoDatabase: process.env.MONGO_DATABASE || 'github-review-point-counter'
   })
   .then(({ server }) => {
     server.listen(process.env.PORT || 3000)
